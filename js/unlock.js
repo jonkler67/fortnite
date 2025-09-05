b09c8961e7a8b6cb617488e06a1d41cee8536657
Augustus-Borchelt <152639143+Augustus-Borchelt@users.noreply.github.com>
2025-09-05 13:16:29 -0500

    Update unlock.js

--- js/unlock.js
+++ js/unlock.js
@@ -1,1 +1,12 @@
+const form = document.getElementById("passwordForm")
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const data = new FormData(form);
+  for (const [name,value] of data) {
+    if(name == "password" && value == "12345679"){
+        location.href = "/pages/supersecret.html"
+    }
+  }
+})
 
