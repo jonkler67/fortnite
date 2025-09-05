835378a564a7b37196a56e5dbbc263d5fce025ae
Augustus-Borchelt <152639143+Augustus-Borchelt@users.noreply.github.com>
2025-09-04 21:35:56 -0500

    Add files via upload

---
+++ js/navButton.js
@@ -0,0 +1,17 @@
+let navButtons = document.getElementsByClassName("title-button");
+
+console.log(navButtons)
+for (let i = 0; i < navButtons.length; i++) {
+    navButtons[i].addEventListener('click', () => {
+        let newLocation = "/pages/" + navButtons[i].id + ".html";
+        if (navButtons[i].id == "index") {
+            location.href = "/"
+        } else if (newLocation != "/pages/.html") {
+            location.href = newLocation
+        }
+
+    })
+}
+
+
+
