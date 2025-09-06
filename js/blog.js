b5168ee5eddc7f97cddbfc11400476c5c9237705
Augustus-Borchelt <152639143+Augustus-Borchelt@users.noreply.github.com>
2025-09-05 21:05:24 -0500

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,7 @@
 const blogList = document.getElementById("blog-list")
 
 
-fetch('/blog/blogs.json')
+fetch('/pages/blog/blogs.json')
   .then(response => {
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
