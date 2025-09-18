e99888db7b5eb5078edc5ea4bcbe6ea248392ca8
Augustus-Borchelt <152639143+Augustus-Borchelt@users.noreply.github.com>
2025-09-18 01:28:46 -0500

    They're watching

---
+++ js/corruption.js
@@ -0,0 +1,41 @@
+let opacity = 0;
+let corruptionWall = document.createElement("div")
+corruptionWall.style = `
+height: 100%; 
+width: 100%; 
+background-image: url("https://media.tenor.com/GY5ihAZWNWEAAAAM/ojos.gif");
+opacity: 0%;
+position: absolute;
+z-index: 100;
+pointer-events: none;
+`
+corruptionWall.id = "evil"
+let bodymaker = document.createElement('span')
+bodymaker.innerText = "hi"
+bodymaker.style = "display: block; style:hidden; height: 100vh"
+document.getElementsByTagName("body")[0].prepend(corruptionWall)
+
+function evilLoop() {
+    if (opacity < 100) {
+        setTimeout(() => {
+            let randNum = Math.floor(Math.random() * (2 - 0 + 1)) + 0
+            if (randNum < 2) {
+                opacity += 0.25
+            } else {
+                opacity -= 0.25
+            }
+            console.log(opacity)
+            document.getElementById("evil").style = `
+                height: 100%; 
+                width: 100%; 
+                background-image: url("https://media.tenor.com/GY5ihAZWNWEAAAAM/ojos.gif");
+                opacity: ${opacity}%;
+                position: absolute;
+                z-index: 100;
+                pointer-events: none;
+            `
+            evilLoop()
+        }, 500)
+    }
+}
+evilLoop()
