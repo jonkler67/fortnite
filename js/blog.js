af86c7bdff8ec6ae1e0ecac54a69f14e0aaf8e04
Augustus-Borchelt <152639143+Augustus-Borchelt@users.noreply.github.com>
2025-09-18 01:00:03 -0500

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -10,17 +10,28 @@ fetch('/pages/blog/blogs.json')
   })
   .then(data => {
     console.log(data); // The parsed JavaScript object
-    for(let i = 0; i < data["blogs"].length; i++){
+    let latestBlog;
+    let latestBlogDate = "1/1/1";
+    for (let i = 0; i < data["blogs"].length; i++) {
       let blog = data["blogs"][i]
       console.log(blog)
       blogList.innerHTML += newBlogElement(blog["name"], blog["date"])
-    } //GITHUB WILL NOT UPDATE THIS FILE HOPEFULLY THIS COMMENT FORCES GITHUB TO DO IT
+      if (latestBlogDate == null || new Date(latestBlogDate).getTime() < new Date(blog["date"]).getTime()) {
+        latestBlog = blog["name"]
+        latestBlogDate = blog["date"]
+      }
+    }
+    let link = document.createElement('a')
+    link.style = "font-size: 3vh; background-color:black; width: 33%; margin: 0 auto;"
+    link.href = `https://jonkler67.gay/pages/blog/${latestBlog}.html`
+    link.innerText = `${latestBlog} Created On: ${latestBlogDate}`
+    document.getElementById("newest-blog").appendChild(link)
   })
   .catch(error => {
     console.error('Error fetching or parsing JSON:', error);
   });
 
 
-function newBlogElement(name, date){
+function newBlogElement(name, date) {
   return `<a href='https://jonkler67.gay/pages/blog/${name}.html'>${name} Created On: ${date}</a>`
 }
